0dfeb4fd4f71edf7e3e4d7123ba5d564e88475a3
Adanwa Nwobodo <adanwanwobodo@gmail.com>
2022-12-07 07:09:39 -0800

    add mobile menu

---
+++ script/hamburger.js
@@ -0,0 +1,14 @@
+const icon = document.getElementById("icon");
+const icon1 = document.getElementById("a");
+const icon2 = document.getElementById("b");
+const icon3 = document.getElementById("c");
+const nav = document.getElementById('navbar');
+const blue = document.getElementById("blue");
+
+icon.addEventListener('click', function() {
+  icon1.classList.toggle('a');
+  icon2.classList.toggle('c');
+  icon3.classList.toggle('b');
+  nav.classList.toggle('show');
+  blue.classList.toggle('slide');
+});
